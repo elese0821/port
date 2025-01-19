80f3840357e74617430cee74a723e558e5c4da2d
원영 <elese0821@naver.com>
2025-01-20 00:39:36 +0900

    4

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // 배포 환경에서 상대 경로 사용
+  build: {
+    outDir: 'dist',
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:5050',
